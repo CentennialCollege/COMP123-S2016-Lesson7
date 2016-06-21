9d1d661ee5569b1ceaacb0ba6fae1a48b18da338
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-06-21 14:24:31 -0400

    updated project

--- COMP123-S2016-Lesson7/Program.cs
+++ COMP123-S2016-Lesson7/Program.cs
@@ -10,6 +10,38 @@ namespace COMP123_S2016_Lesson7
     {
         static void Main(string[] args)
         {
+            Random random = new Random(); // pseudo number object
+            List<int> diceRolls = new List<int>();
+            int[] tally = new int[13];
+            int maxRolls = 5000000;
+
+            // builds the list
+            for (int index = 0; index < maxRolls; index++)
+            {
+                int firstDie = random.Next(1, 7);
+                int secondDie = random.Next(1, 7);
+                int dice = firstDie + secondDie;
+                tally[dice]++;
+                diceRolls.Add(dice);
+            }
+
+            diceRolls.Sort();
+
+            // display list
+            /*
+            foreach (int result in diceRolls)
+            {
+                Console.Write(result + " ");
+            }
+            Console.WriteLine();
+            */
+
+            for (int index = 2; index < 13; index++)
+            {
+                Console.WriteLine("{0}: {1} ",index, tally[index]);
+            }
+
+            
         }
     }
 }
